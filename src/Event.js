80feef88630d796b11fb1b2f255b81f8d277a73e
kimkwanka <kimkwanka@gmail.com>
2021-07-30 09:51:27 +0200

    test: add Event component tests

--- src/Event.js
+++ src/Event.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Event = () => (
-  <div className="event" />
-);
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+  return (
+    <div className="event">
+      <h3 className="event-title">{event.summary}</h3>
+      <p className="event-start">{event.start.dateTime}</p>
+      <p className="event-location">{`@${event.summary} | ${event.location}`}</p>
+      {showDetails && (
+        <div className="event-details">
+          <h3>About the event:</h3>
+          <a className="event-link" href={event.htmlLink}>See details on Google Calendar</a>
+          <p className="event-description">{event.description}</p>
+        </div>
+      )}
+      <button
+        type="button"
+        className="event-details-button"
+        onClick={toggleDetails}
+      >
+        {showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
+    </div>
+  );
+};
 
 export default Event;
---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,37 @@
+/* global describe, test, expect, beforeAll */
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Event from '../Event';
+
+import mockData from '../mock-data';
+
+describe('<Event /> component', () => {
+  let EventWrapper = {};
+
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={mockData[0]} />);
+  });
+
+  test('renders basic event information', () => {
+    const event = mockData[0];
+
+    expect(EventWrapper.find('.event-title').text()).toBe(event.summary);
+    expect(EventWrapper.find('.event-start').text()).toBe(event.start.dateTime);
+    expect(EventWrapper.find('.event-location').text()).toBe(`@${event.summary} | ${event.location}`);
+    expect(EventWrapper.find('.event-details').length).toBe(0);
+  });
+
+  test('renders detailed event information upon clicking the details button', () => {
+    const event = mockData[0];
+
+    expect(EventWrapper.find('.event-details').length).toBe(0);
+
+    EventWrapper.find('.event-details-button').simulate('click');
+
+    expect(EventWrapper.find('.event-details').length).toBe(1);
+
+    expect(EventWrapper.find('.event-link').prop('href')).toBe(event.htmlLink);
+    expect(EventWrapper.find('.event-description').text()).toBe(event.description);
+  });
+});
