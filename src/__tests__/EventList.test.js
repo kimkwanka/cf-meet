1ecbffe0eea082060ffd4195ed519dce351e9ac4
kimkwanka <kimkwanka@gmail.com>
2021-07-30 09:11:47 +0200

    test: update EventList test to use mock data

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -2,14 +2,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import mockData from '../mock-data';
+
 import EventList from '../EventList';
 import Event from '../Event';
 
 describe('<EventList /> component', () => {
   test('render correct number of events', () => {
     const EventListWrapper = shallow(
-      <EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />,
+      <EventList events={mockData} />,
     );
-    expect(EventListWrapper.find(Event)).toHaveLength(4);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
   });
 });
