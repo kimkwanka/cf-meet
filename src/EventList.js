358b85bb74a008eda61310621762d14fca32eabb
Kim Kwanka <kimkwanka@gmail.com>
2021-08-14 10:16:29 +0200

    feat: add offline warning

--- src/EventList.js
+++ src/EventList.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import Event from './Event';
 
+import { WarningAlert } from './Alert';
+
 const EventList = ({ events }) => (
   <ul className="event-list">
-    {events.map(event => <li key={event.id}><Event event={event} /></li>)}
+    {!navigator.onLine && <WarningAlert text="App is offline, cached event list has been loaded." />}
+    {events.map((event) => <li key={event.id}><Event event={event} /></li>)}
   </ul>
 );
 
