621b2cf002010125e05247ebf549555484b32df0
kimkwanka <kimkwanka@gmail.com>
2021-09-28 19:12:36 +0200

    fix: improve EventGenre layout

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import {
-  PieChart,
-  Pie,
-  Cell,
-  ResponsiveContainer,
+  PieChart, Pie, Cell, ResponsiveContainer,
 } from 'recharts';
 
 const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
@@ -14,24 +13,38 @@ const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 const RADIAN = Math.PI / 180;
 
 const renderCustomizedLabel = ({
-  name, cx, cy, midAngle, innerRadius, outerRadius, percent, index,
+  name,
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
 }) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const radius = innerRadius + (outerRadius - innerRadius) * 1.2;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
   return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+    <text
+      x={x}
+      y={y}
+      fill="black"
+      textAnchor="middle"
+      dominantBaseline="central"
+    >
       {`${name} ${(percent * 100).toFixed(0)}%`}
     </text>
   );
 };
 
 const EventGenre = ({ events }) => {
-  const data = genres.map((genre) => {
-    const value = events.filter(({ summary }) => summary.includes(genre)).length;
-    return { name: genre, value };
-  });
+  const data = genres
+    .map((genre) => {
+      const value = events.filter(({ summary }) => summary.includes(genre)).length;
+      return { name: genre, value };
+    })
+    .filter(({ value }) => value > 0);
 
   return (
     <ResponsiveContainer height={400}>
@@ -46,13 +59,17 @@ const EventGenre = ({ events }) => {
           fill="#8884d8"
           dataKey="value"
         >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          {data.map(({ name }, index) => (
+            <Cell key={`cell-${name}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
       </PieChart>
     </ResponsiveContainer>
   );
+};
+
+EventGenre.propTypes = {
+  events: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
 export default EventGenre;
